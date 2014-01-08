a19f32f5d6eacad52c8eb452c39ade61a85ab43d
burak-er <burak.er@btu.edu.tr>
2014-01-08 11:36:40 +0200

    Addition of 2nd assingment problem statement

--- Vibration in Engineering/Assignment-II/Matlab/odefunc.m
+++ Vibration in Engineering/Assignment-II/Matlab/odefunc.m
@@ -1,13 +1,11 @@
 function ydot=odefunc(t,y)
-global i;
-global tval;
+global i y2val tval;
 kt=200000;
 a=30/100;
 b=5/100;
 V=60000/3600;
 halfperiod=a/V;
 y1=0;
-global y2val;
 y2=b*sin(pi*t/halfperiod)*(t<halfperiod);
 i=i+1;
 y2val(i)=y2*kt;
@@ -19,6 +17,5 @@ C=[400 -10 -200 -200;-10 210.5 -140 150;-200 -140 200 0;-200 150 0 200;];
 disp=y(1:4);
 vel=y(5:8);
 ydot(1:4)=vel';
-ydot(5:8)=(inv(M))*(-C*vel-K*disp+F1);
-ydot=ydot';
+ydot(5:8)=((inv(M))*(-C*vel-K*disp+F1))';
 end
